94f98294ddc0f59c39bfae837f3a0560aaf60858
张帅 <47773565+254558@users.noreply.github.com>
2026-06-09 14:39:28 +0800

    Initial Commit

---
+++ trans/transApp.swift
@@ -0,0 +1,17 @@
+//
+//  transApp.swift
+//  trans
+//
+//  Created by 张帅 on 2026/6/9.
+//
+
+import SwiftUI
+
+@main
+struct transApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ transTests/transTests.swift
@@ -0,0 +1,19 @@
+//
+//  transTests.swift
+//  transTests
+//
+//  Created by 张帅 on 2026/6/9.
+//
+
+import Testing
+@testable import trans
+
+struct transTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
